4dd6ba24c77150b3f6802f96afa9d9fda9af96ee
S. Avtar Singh <singh.avtar_92@hotmail.com>
2025-09-01 23:50:58 -0400

    Restructure Shared package for proper namespace

--- pqc_lib/kdf.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def kdf_shake256_32(*parts: bytes) -> bytes:
-    """Derive a 256-bit key from multiple byte inputs using SHAKE256."""
-    shake = hashlib.shake_256()
-    for p in parts:
-        if p:
-            shake.update(p)
-    return shake.digest(32)
